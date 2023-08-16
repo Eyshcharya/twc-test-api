46cbc354594cdbed342a06d32d7c5cd8b2154f70
Eyshcharya <eyshcharya@gmail.com>
2023-08-17 02:28:14 +0530

    Added Cors

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import router from './Routes/route.js';
+import cors from 'cors';
 import connectDB from './DB/db.js';
 connectDB();
 import { config } from 'dotenv';
 config();
 
 const app = express();
+
+// to access req.body and encoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
 // router
 app.use('/', router);
