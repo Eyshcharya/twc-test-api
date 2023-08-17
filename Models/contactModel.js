9b964c9acc4f3d85f6a6bcda28990c32c9809769
Eyshcharya <eyshcharya@gmail.com>
2023-08-17 16:25:57 +0530

    Implemented contact model

--- Models/contactModel.js
+++ Models/contactModel.js
@@ -1,22 +1,11 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
+const contactSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: true },
+  phone: { type: Number, required: true },
+  gender: { type: String, required: true },
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-const User = mongoose.model('user', userSchema);
-export { User };
+const Contact = mongoose.model('contact', contactSchema);
+export { Contact };
