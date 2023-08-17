7217eab2c70985b601441ec9886afa0100b0c409
Eyshcharya <eyshcharya@gmail.com>
2023-08-17 16:26:46 +0530

    Added register route

--- Routes/route.js
+++ Routes/route.js
@@ -1,4 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
+import { userRegister } from '../Controllers/actionController.js';
+
+// action routes
+router.post('/register', userRegister);
 export default router;
