2670843e82de318c3876b1aea29ef3fa22a15fd7
Eyshcharya <eyshcharya@gmail.com>
2023-08-31 14:26:41 +0530

    Added enum for gender

--- Models/contactModel.js
+++ Models/contactModel.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
   email: { type: String, required: true },
   name: { type: String, required: true },
-  phone: { type: Number, required: true, unique: true },
-  gender: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  gender: { type: String, required: true, enum: ["male", "female"] },
 });
 
-const Contact = mongoose.model('contact', contactSchema);
+const Contact = mongoose.model("contact", contactSchema);
 export { Contact };
